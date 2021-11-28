4343fe851b060be1e7994d55978b247a03142302
Furkan Atılgan <atilganyf@gmail.com>
2021-11-28 08:27:12 +0300

    api key fix

--- src/components/GoogleMapComponent.js
+++ src/components/GoogleMapComponent.js
@@ -2,13 +2,14 @@ import React from "react"
 import compose from "recompose/compose";
 import withProps from "recompose/withProps";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { API_KEY } from "./env"
 
 const GoogleMapComponent = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places=AIzaSyDJTk3nG9zcwqdZlGqweq9fh2JbYR7v1ls",
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
         loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `330px`, borderRadius:"10px" }} />,
-        mapElement: <div style={{ height: `100%`, borderRadius:"7px" }} />,
+        containerElement: <div style={{ height: `330px`, borderRadius: "10px" }} />,
+        mapElement: <div style={{ height: `100%`, borderRadius: "7px" }} />,
     }),
     withScriptjs,
     withGoogleMap
